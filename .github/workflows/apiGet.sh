48d09487cfacd40f63fdd1819fd4c45873210bca
Niki <43937352+NikiJD@users.noreply.github.com>
2022-06-03 15:58:42 -0700

    removed 'data-raw'

--- .github/workflows/apiGet.sh
+++ .github/workflows/apiGet.sh
@@ -3,7 +3,6 @@
 chmod +x script.sh
 
 result = curl --location --request GET 'https://content.libraries.wsu.edu/digital/bl/dmwebservices/index.php?q=dmQuery/senex/0/0/nosort/1024/10/0/0/0/json' \
---header 'Content-Type: application/json' \
---data-raw 
+--header 'Content-Type: application/json'
 
 $result >> result.json
