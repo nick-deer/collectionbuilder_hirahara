7b3a6ca685344cdda5e24f868aca1afc3bcbb0f9
Niki <43937352+NikiJD@users.noreply.github.com>
2022-06-08 13:13:21 -0700

    Delete apiGet.sh

--- .github/workflows/apiGet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chmod +x script.sh
-
-result = curl --location --request GET 'https://content.libraries.wsu.edu/digital/bl/dmwebservices/index.php?q=dmQuery/senex/0/0/nosort/1024/10/0/0/0/json' \
---header 'Content-Type: application/json'
-
-$result >> result.json
